725770be005ab78c17d415ae72a6c14292ca3cee
SergeantShadoww <47951918+SergeantShadoww@users.noreply.github.com>
2020-05-01 03:07:28 +0100

    Finalised blacklist and reply system.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const color = require("colors");
 const figlet = require("figlet");
 const customId = require("custom-id");
 const { token } = require("./secrets/token.json")
+const { black, red_light } = require("./colors.json")
 
 // Below we are telling the bot what to do on the 'ready' event.
 client.on('ready', () => {
@@ -13,7 +14,7 @@ client.on('ready', () => {
 })
 
 client.on('message', async (message) => {
-    console.log("Active")
+    console.log("Active");
 
     //If the author of the message is a bot then the bot will ignore the message.
     if (message.author.bot) return;
@@ -23,7 +24,11 @@ client.on('message', async (message) => {
     let command = messageArray[0].toLowerCase();
     let args = messageArray.slice(1);
 
-    var id = customId({ randomLength: 1 })
+    var id = customId({ randomLength: 1 });
+    /* Below we are doing a check to see if the user has a document in the database.
+       If the user does not have a document then one will be made, the default values for toggleventing and blacklisted is false.
+       We are using await so that the check can be completed before another check is started on line 135, if the document doesn't exist then it will error.
+    */
     await db.collection('clients').doc(message.author.id).get()
         .then((doc) => {
             if (!doc.exists) {
@@ -47,14 +52,29 @@ client.on('message', async (message) => {
         if (message.content.startsWith(`${prefix}blacklist`)) {
             // The bot will search the array of authorised ids and look to see if the authors id is in there, if it isn't then the bot will return.
             if (!config.ownerID.includes(message.author.id)) return message.channel.send("You're not a bot developer!");
+
             /* The bot is now checking to see if a userID has been specified.
                 If a userID has not been specified the bot will return and notify the user.
             */
             if (!args[0]) {
                 return message.channel.send("You must supply a user ID to blacklist.")
             } else {
+                /* We will be using the global boolean nf as a way to identify whether or not the document was found within the databse.
+                   If the boolean is false then the program knows that the document doesn't exist and will not continue the blacklist operation.
+                   If the boolean is true then the program will know that a document was found and that it is okay to carry on.
+                */
                 global.nf = true
+
+                /* The global variable bid will be used so that the bot knows which person to send a DM to once they have blacklisted the ID.
+                   At the start of the process the variable is set to nothing so that the ID from the previous operation isn't carried over.
+                */
                 global.bid = ""
+
+                /* Below is where we will attempt to search all documents in the collection and try to find a document which has an ID matching the specified one.
+                   If a matching document isn't found then the program will return and the message author will be notified.
+                   If a matching document is found, the program will then set the Blacklisted boolean to true.
+                   Once the Blaclisted boolean has been set to true the bot will then grab the users ID from the document name and send a DM to that user notifying them that they've been blacklisted.
+                */
                 let reference = db.collection("clients")
                 let queryRef = db.collection("clients").where("VentingID", "==", args[0]).get()
                     .then(snapshot => {
@@ -65,17 +85,89 @@ client.on('message', async (message) => {
 
                         snapshot.forEach(doc => {
                             global.bid = doc.id
+                            let reason = args.slice(1).join(" ");
+                            if (!reason) {
+                                global.nf = false;
+                                return message.channel.send(`**ERROR**: Failed to balacklist \`${args[0]}\`, please supply a reason.`)
+                            }
+                            if (doc.data().Blacklisted) {
+                                global.nf = false;
+                                return message.channel.send(`**ERROR**: \`${args[0]}\` is already blacklisted.`)
+                            };
                             db.collection("clients").doc(doc.id).update({ Blacklisted: true })
                         });
                     })  
                     .then(() => {
                         if (!global.nf) return;
-                        client.users.get(global.bid).send(`You have been blacklisted from using SafeVenting by: \`${message.author.name}\`.`)
-                        
-                        return message.channel.send(`Success: \`${args[0]}\` has been blacklisted.`)
+                        let reason = args.slice(1).join(" ")
+                        let embed = new MessageEmbed()
+                        .setColor(red_light)
+                        .setTitle(`You have been blacklisted from using SafeVenting.`)
+                        .setDescription(`Moderator: \`${message.author.username}\`. \nReason: ${reason}.`)
+                        .setTimestamp()
+                        .setFooter('SafeVenting, a bot created with love by MrShadow.')
+
+                        client.users.cache.get(global.bid).send(embed)
+                        return message.channel.send(`Success: \`${args[0]}\` has been blacklisted.`);
                     })
                     .catch(err => {
-                        console.log(err)
+                        console.err(err);
+                        return message.channel.send(`**ERROR**: An unexpected error has occured, please contact MrShadow **immediately**.`)
+                    })
+            }
+        }
+
+        if (message.content.startsWith(`${prefix}whitelist`)) {
+            // Add some code here later.
+        }
+
+        if (message.content.startsWith(`${prefix}r`)) {
+            /* The bot is now checking to see if a userID has been specified.
+                If a userID has not been specified the bot will return and notify the user.
+            */
+            if (!args[0]) {
+                return message.channel.send('You must supply an ID to reply to.');
+            } else {
+                /* We will be using the global boolean nf2 as a way to identify whether or not the document was found within the databse.
+                   If the boolean is false then the program knows that the document doesn't exist and will not continue the reply operation.
+                   If the boolean is true then the program will know that a document was found and that it is okay to carry on.
+                */
+                global.nf2 = true;
+
+                /* The global variable bid will be used so that the bot knows which person to send a DM to once they have blacklisted the ID.
+                   At the start of the process the variable is set to nothing so that the ID from the previous operation isn't carried over.
+                */
+                global.rid = "";
+
+                let reference = db.collection("clients");
+                let queryRef = db.collection("clients").where("VentingID", "==", args[0]).get()
+                    .then(snapshot => {
+                        if (snapshot.empty) {
+                            global.nf2 = false;
+                            return message.channel.send(`**ERROR**: Failed to find \`${args[0]}\` in the database, are you sure this ID exists?`)
+                        };
+
+                        snapshot.forEach(doc => {
+                            if (!doc.data().ToggleVenting) return message.channel.send(`**ERROR**: Failed to send a message to \`${args[0]}\`, they have SafeVenting toggled off.`)
+                            global.rid = doc.id
+                        })
+                    })
+                    .then(() => {
+                        if (!global.nf2) return;
+                        let text = args.slice(1).join(" ");
+                        if (!text) return message.channel.send(`**ERROR**: Failed to send a message to \`${args[0]}\`, you must give me some text to send.`)
+                        let embed = new MessageEmbed()
+                        .setColor(black)
+                        .setTitle(`By ${message.author.tag}`)
+                        .setDescription(`${text}`)
+                        .setTimestamp()
+                        .setFooter('SafeVenting, a bot created with love by MrShadow.')
+
+                        return client.users.cache.get(global.rid).send(embed)
+                    })
+                    .catch(err => {
+                        console.err(err);
+                        return message.channel.send(`**ERROR**: An unexpected error has occured, please contact MrShadow **immediately**.`)
                     })
             }
         }
@@ -88,8 +180,18 @@ client.on('message', async (message) => {
             db.collection("clients").doc(message.author.id).get()
                 .then(async(doc) => {
                     let id = doc.data().VentingID
+                    
+                    /* Below we have 2 checks, first we have a check to see if the user is blacklisted.
+                       The users document will be checked in the database and if Blacklisted is true then the bot will return.
+                       The second check will check to see if the user has already enabled venting, if they have the bot will return and notify the user.
+                    */
                     if (doc.data().Blacklisted) return message.channel.send('You have been blacklisted from using SafeZone Venting.')
                     if (doc.data().ToggleVenting) return message.channel.send('You have already enabled venting! To disable it please type: `disableVenting`.')
+
+                    /* If the two checks above have both returned false,
+                       the bot will update the ToggleVenting boolean in the database to true.
+                    */
+
                     await db.collection("clients").doc(message.author.id).update({
                         ToggleVenting: true
                     });
